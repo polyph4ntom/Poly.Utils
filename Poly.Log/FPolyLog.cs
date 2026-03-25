39a0693c73969b1d6088a5076352bd2f0509032c
Sebastian Adamowski <s.adamowski94@gmail.com>
2026-03-25 17:29:32 +0100

    Added the ability to hide wrapper class in call stack

--- Poly.Log/FPolyLog.cs
+++ Poly.Log/FPolyLog.cs
@@ -8,6 +8,7 @@ namespace Poly.Log
 	{
 		private const string POLY_LOG = "WITH_POLY_LOG";
 		
+		[HideInCallstack]
 		private static void Message(string category, LogType logType, string message,
 			UnityEngine.GameObject context = null)
 		{
@@ -19,24 +20,28 @@ namespace Poly.Log
 			Debug.LogFormat(logType, LogOption.None, context, $"[{category}] {message}");
 		}
 
+		[HideInCallstack]
 		[Conditional(POLY_LOG)]
 		public static void Log(string category, string message, UnityEngine.GameObject context = null)
 		{
 			Message(category, LogType.Log, message, context);
 		}
 
+		[HideInCallstack]
 		[Conditional(POLY_LOG)]
 		public static void Warning(string category, string message, UnityEngine.GameObject context = null)
 		{
 			Message(category, LogType.Warning, message, context);
 		}
 
+		[HideInCallstack]
 		[Conditional(POLY_LOG)]
 		public static void Error(string category, string message, UnityEngine.GameObject context = null)
 		{
 			Message(category, LogType.Error, message, context);
 		}
 
+		[HideInCallstack]
 		[Conditional(POLY_LOG)]
 		public static void Assert(string category, string message, UnityEngine.GameObject context = null)
 		{
